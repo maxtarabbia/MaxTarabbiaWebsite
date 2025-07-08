f55d571a6ec45fe2e753f1714adfeed6649bf950
maxtarabbia <maxtarabbia@icloud.com>
2025-07-08 07:50:20 -0500

    Made images portfolio image clickable

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 // Find the button by its ID
 const redirectButton = document.getElementById('redirectButton');
 
@@ -5,10 +6,4 @@ const redirectButton = document.getElementById('redirectButton');
 redirectButton.addEventListener('click', function() {
     console.log("Redirecting..."); // A message for you in the browser's console
     window.location.href = "https://www.google.com"; // The destination URL
-});
-
-const expandableimage = document.getElementById('portfolio-item');
-
-expandableimage.addEventListener('click', function() {
-    expandableimage.classList.toggle('expanded');
 });
